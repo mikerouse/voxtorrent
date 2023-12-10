79973767aad37e77bca43821c4655d3a171e0c57
Mike Rouse <mike@mikerouse.uk>
2023-12-10 15:34:30 +0000

    Make progress on #11

--- resources/views/livewire/torrents/all-torrents-list.blade.php
+++ resources/views/livewire/torrents/all-torrents-list.blade.php
@@ -1,21 +1,25 @@
 <div class="space-y-4">
     @foreach($torrents as $torrent)
-        <div class="p-4 border rounded shadow bg-white dark:bg-gray-800">
+        <div class="p-4 border rounded bg-white dark:bg-gray-800">
             <h3 class="text-xl">
                 <a href="#" class="font-semibold text-blue-600 hover:text-blue-800 dark:text-blue-300 dark:hover:text-blue-500">{{ $torrent->name }}</a>
             </h3>
             <p class="mt-2 text-gray-600 dark:text-gray-400">{{ $torrent->description }}</p>
             <div class="mt-4 space-x-2">
                 <button class="inline-flex items-center space-x-1 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300">
-                    <i class="fas fa-thumbs-up"></i>
+                    <i class="fas fa-smile fa-lg text-green-500"></i>
                     <span>{{ number_format($torrent->likes) }}</span>
                 </button>
                 <button class="inline-flex items-center space-x-1 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300">
-                    <i class="fas fa-thumbs-down"></i>
+                    <i class="fas fa-meh fa-lg text-yellow-500"></i>
+                    <span>{{ number_format($torrent->neutralFaces) }}</span>
+                </button>
+                <button class="inline-flex items-center space-x-1 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300">
+                    <i class="fas fa-frown fa-lg text-orange-500"></i>
                     <span>{{ number_format($torrent->dislikes) }}</span>
                 </button>
                 <span class="inline-flex items-center space-x-1 text-gray-500 dark:text-gray-400">
-                    <i class="fas fa-pen-fancy"></i>
+                    <i class="fas fa-pen-fancy fa-lg text-blue-500"></i>
                     <span>{{ number_format($torrent->signatures_count) }}</span>
                 </span>
             </div>
