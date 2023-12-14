5e52faa5cf840f205de7c4a729dd32dab363f8df
Mike Rouse <mike@mikerouse.uk>
2023-12-14 22:08:55 +0000

    CSS and JS tweaks

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,1 @@
 import './bootstrap';
-
-// I only want this to run on the create torrent page
-if (document.getElementById('createTorrent')) {
-    console.info('createTorrent page detected');
-} else {
-    console.error('createTorrent page: Livewire is not loaded yet');
-}
