fab0801a85b830a6043793ec1dc5bd7793fa61e4
Mike Rouse <mike@mikerouse.uk>
2023-12-16 19:06:15 +0000

    stub out some mayor basics

---
+++ app/Http/Controllers/MayorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreMayorRequest;
+use App\Http\Requests\UpdateMayorRequest;
+use App\Models\Mayor;
+
+class MayorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMayorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Mayor $mayor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Mayor $mayor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMayorRequest $request, Mayor $mayor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Mayor $mayor)
+    {
+        //
+    }
+}
