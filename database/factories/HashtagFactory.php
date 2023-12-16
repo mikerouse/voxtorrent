96c54c62f3a56b9afae92e94632888015a5e18ca
Mike Rouse <mike@mikerouse.uk>
2023-12-16 19:10:48 +0000

    additional factories

--- database/factories/HashtagFactory.php
+++ database/factories/HashtagFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Hashtags>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Hashtag>
  */
-class HashtagsFactory extends Factory
+class HashtagFactory extends Factory
 {
     /**
      * Define the model's default state.
