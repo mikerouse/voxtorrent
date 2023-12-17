ae026ee5fda1fdd5cc839fb94a8e47f8e34e2ebd
Mike Rouse <mike@mikerouse.uk>
2023-12-17 17:39:08 +0000

    UI tweak

--- resources/views/livewire/torrents/components/torrent-content.blade.php
+++ resources/views/livewire/torrents/components/torrent-content.blade.php
@@ -1,4 +1,8 @@
 <div class="text-gray-600 dark:text-gray-400 text-lg bg-white dark:bg-gray-800 p-4">
-    <h3 class="dark:text-white text-gray-400 font-extrabold text-xl mb-4">{{ $torrent->name }}</h3>
-    {{ $torrent->description }}
+    <h3 class="dark:text-white text-gray-400 font-extrabold text-xl mb-4">
+        {{ $torrent->name }}
+    </h3>
+    <div>
+        {{ $torrent->description }}
+    </div>
 </div>
